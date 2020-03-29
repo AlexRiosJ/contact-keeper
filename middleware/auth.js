387d2ef1aa2e1ad3def907e21bf77688adde8631
Alejandro Ríos Jasso <jassoalex97@hotmail.com>
2020-03-29 02:18:06 -0600

    removing console.log

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,17 +3,18 @@ const config = require('config');
 
 module.exports = function(req, res, next) {
 	// Get token from the header
-    const token = req.header('x-auth-token');
-    console.log(token);
+	const token = req.header('x-auth-token');
 
 	// Check if there is no token
 	if (!token) {
-		return res.status(401).json({ msg: 'Authorization denied: token is missing' });
+		return res
+			.status(401)
+			.json({ msg: 'Authorization denied: token is missing' });
 	}
 
 	try {
 		const decoded = jwt.verify(token, config.get('jwtSecret'));
-        req.user = decoded.user;
+		req.user = decoded.user;
 		next();
 	} catch (err) {
 		res.status(401).json({ msg: 'Token is not valid' });
